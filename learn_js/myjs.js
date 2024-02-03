fd1fd0a05f0cda9eedba55f0d04bce7effb64c12
Amrit kumar Bhardwaj <139677817+amrit-java@users.noreply.github.com>
2024-02-04 00:42:41 +0530

    Add files via upload

---
+++ learn_js/myjs.js
@@ -0,0 +1,15 @@
+//alert("this is my file")
+//document.write(5-2);
+//window.alert("hi i am amrit");
+//document.write(prompt("this is name"))
+////////////////////////////////////////////////
+//name=prompt("enter name");
+//document.write(name);
+//////////////////////////////////////////////
+//confirm("would you like to exit")
+//document.write(confirm("would you like to exit"))
+//////////////////Dynemic valu//////////////////////////////////////////////////
+<h1 id="my-content"> </h1>
+document.getElementById("my-content").innerHTML = "my name is Amrit"
+
+/////////////////////////////Variable///////////////////////////////////////////
